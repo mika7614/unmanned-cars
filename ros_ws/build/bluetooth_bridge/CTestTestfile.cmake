c2ea63a06488d9729e84fb20aeaf42548d1cd2a6
mika7614 <68652647+mika7614@users.noreply.github.com>
2024-05-23 20:27:16 +0800

    Add files via upload

---
+++ ros_ws/build/bluetooth_bridge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/ros_ws/src/bluetooth_bridge
+# Build directory: /home/pi/ros_ws/build/bluetooth_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
